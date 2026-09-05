eecd91165ec9bc1b545d44ced2dfedc0e1c1f5a5
Six <six.workmotion@gmail.com>
2026-09-06 05:01:21 +0700

    feat: add dynamic export to sitemap for improved rendering

--- apps/web/src/app/sitemap.ts
+++ apps/web/src/app/sitemap.ts
@@ -2,6 +2,8 @@ import type { MetadataRoute } from 'next'
 import { getPublicContentSitemap } from '@/controllers/content.controller'
 import { SITE_CONFIG } from '@/site.config'
 
+export const dynamic = 'force-dynamic'
+
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
   const staticPages: MetadataRoute.Sitemap = [
     {
